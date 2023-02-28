48265af8b0d32d839d5701eb1c07d6131a33a548
Juggernautbotmx <78008570+Juggernautbotmx@users.noreply.github.com>
2023-02-27 21:21:49 -0600

    Create Embeds.py

---
+++ Embeds.py
@@ -0,0 +1,11 @@
+
+embed = discord.Embed(title="Nike upcoming",
+                      url="Nike Dunk Low SE  Industrial Blue",
+                      description="Región :🇲🇽 [Mexico]\n\nSku : DV0834-101\n\nFecha de lanzamiento: 01/03/2023. 8:00Am\n\nStatus: ACTIVE :green_circle:\n\nTipo de lanzamiento: Flow",
+                      colour=0x52d6fc)
+
+embed.set_image(url="https://media.discordapp.net/attachments/802071078503448576/1079956078403530843/IMG_0047.jpg")
+
+embed.set_footer(text="Juggernaut MX.")
+
+await ctx.send(embed=embed
